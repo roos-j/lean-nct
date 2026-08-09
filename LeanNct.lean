374657bc71a165f267101d35e4f4a561add6ce3f
Joris Roos <jroos.math@gmail.com>
2026-08-08 23:36:26 -0400

    fix github actions

--- LeanNct.lean
+++ LeanNct.lean
@@ -1,2 +1,6 @@
-import LeanNct.Introduction
-import LeanNct.MainArgument.MainInduction
+import LeanNct.Preliminaries.MKernels
+
+open Codex.Preliminaries.MKernels
+
+#check cauchySchwarzAtK_bound
+#print axioms cauchySchwarzAtK_bound
