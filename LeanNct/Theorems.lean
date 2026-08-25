7851caea50410ec50067276ef8c4f2c94756e567
Joris Roos <jroos.math@gmail.com>
2026-08-25 15:41:40 -0400

    small changes

--- LeanNct/Theorems.lean
+++ LeanNct/Theorems.lean
@@ -50,14 +50,17 @@ theorem C_main_ergodic_theorem_finite (n : ℕ) (r : ℝ) : C_main_ergodic_theor
 Let $n\ge 2$ be an integer and let $r>2^{n-1}$, or $r\ge 2$ if $n=2$.
 Let $(X,\Sigma,\mu)$ be a $\sigma$-finite measure space,
 let $(T_j)_{j\in [n)}$ be an $n$-tuple of mutually commuting measure
-preserving transformations on $X$, and let $\mathbf{f}=(f_j)_{j\in [n)}$ be an $n$-tuple
+preserving transformations on $X$,
+and let $\mathbf{f}=(f_j)_{j\in [n)}$ be an $n$-tuple
 of complex-valued measurable functions on $X$
-such that $\|f_j\|_{L^{2n}(X)}<\infty$ for all $j\in[n)$. Then $M_N(\mathbf{f})$ is measurable
-with finite $L^2(X)$ norm for every positive integer $N$ and
+such that $\|f_j\|_{L^{2n}(X)}<\infty$ for all $j\in[n)$.
+Then $M_N(\mathbf{f})$ is measurable with
+finite $L^2(X)$ norm for every
+positive integer $N$ and
 $$
  \| M_{N} (\mathbf{f}) \|_{V_{r}(L^2(X))}
-\le C_{\text{Main ergodic theorem},n,r}
- \prod_{j=0}^{n-1} \|f_j\|_{2n},
+\le C_{\ref{thm:ergodicthm},n,r}
+ \prod_{j\in [n)} \|f_j\|_{2n},
 $$
 where the constant is defined in `nCT.C_main_ergodic_theorem`.
 -/
@@ -77,7 +80,7 @@ Let $n\ge 2$ be an integer. For every $J\in\mathbb{N}$,
 positive real numbers $t_0<t_1<\cdots<t_J$, and every
 $n$-tuple of real-valued Schwartz functions
 ${\mathbf f}=(f_i)_{i\in[n)}$ satisfying
-$$\|f_i\|_{2^{i+\min(n-i,2)}}=1$$
+$$\|f_i\|_{2^{\min(n,i+2)}}=1$$
 for $i\in[n)$, we have
 $$\sum_{j\in[J)}\|A_{t_{j+1}}(\mathbf 1_{[0,1]},\mathbf f) -
   A_{t_j}(\mathbf 1_{[0,1]},\mathbf f)\|_{L^2(\mathbb{R}^n)}^2
@@ -96,7 +99,7 @@ for every $j\in [J)$, and in particular measurable with finite $L^2(\mathbb{R}^n
 -/
 theorem main_twisted_theorem (hn : 2 ≤ n) {J : ℕ}
     {t : Fin (J + 1) → ℝ} (ht : ∀ i, 0 < t i) (ht' : StrictMono t)
-    {f : Fin n → 𝓢(ℝ^n, ℝ)} (hf : ∀ i, eLpNorm (f i) (2 ^ (i + min (n - i) 2)) volume = 1) :
+    {f : Fin n → 𝓢(ℝ^n, ℝ)} (hf : ∀ i, eLpNorm (f i) (2 ^ min n (i + 2)) volume = 1) :
     ∑ j : Fin J, eLpNorm (𝐀 (t (j.succ))  (𝟙 (Icc 0 1)) (f ·)
       - 𝐀 (t j.castSucc) (𝟙 (Icc 0 1)) (f ·)) 2 volume ^ 2
         ≤ 2 ^ 666 * J ^ (1 - (2 : ℝ) ^ (-(n : ℝ) + 2)) :=
